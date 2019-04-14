c8843376c8bfd87fc361f9b7caf7f34fce64ba4e
1 <tkomyakov@gmail.com>
2019-04-14 22:30:39 +0500

    rx

--- app/src/main/java/komyakov/tfs19s07/di/DataManager.kt
+++ app/src/main/java/komyakov/tfs19s07/di/DataManager.kt
@@ -4,7 +4,6 @@ import io.reactivex.Completable
 import io.reactivex.Flowable
 import komyakov.tfs19s07.dataProviders.IDataProvider
 import komyakov.tfs19s07.dto.ConsolidatedNewsItem
-import java.util.concurrent.TimeUnit
 
 interface DataManager {
     fun loadAll(): Flowable<List<ConsolidatedNewsItem>>
@@ -25,14 +24,10 @@ class DataManagerImpl(private val dataProvider: IDataProvider) : DataManager {
 
     override fun loadAll(): Flowable<List<ConsolidatedNewsItem>> {
         return dataProvider.loadAllNews()
-            //TODO: remove - долгий запрос
-            .delay(5, TimeUnit.SECONDS)
     }
 
     override fun loadFavorite(): Flowable<List<ConsolidatedNewsItem>> {
         return dataProvider.loadFavoriteNews()
-            //TODO: remove - долгий запрос
-            .delay(5, TimeUnit.SECONDS)
     }
 
 }
